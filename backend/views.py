d6297d8a93e6a58ef04cf71a4978daa403b5d3fe
dartrhevan <vovasatunkin@mail.ru>
2020-06-10 15:32:03 +0500

    simple back filter

--- backend/views.py
+++ backend/views.py
@@ -7,24 +7,51 @@ from flask import render_template
 from app import app
 from flask import request, make_response
 from man import Man
+import re
 
+def_min = 0
+def_max = 110
 
 @app.route('/save', methods=["POST"])
 def save():
+    age
     print(request.form['name'])
-    obj = Man.create(name=request.form['name'], age=request.form['age'])
+    try:
+        age = int(request.form['age'])
+    except ValueError:
+        make_response('{"message":"Incorrect age"}', 400)
+    obj = Man.create(name=request.form['name'], age=age)
+    if age < def_min or age > def_max:
+        return make_response('{"message":"Incorrect age"}', 400)
     #obj.save()
     return make_response("", 200)
 
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+def root():
+    return app.send_static_file('index.html')
 
 
-@app.route('/people-list')
+@app.route('/test')
+def test():
+    return "Hi!"
+
+print(re.findall('a', 'asfra'))
+
+@app.route('/people-list',  methods=["POST"])
 def people_list():
     people = Man.select()
     #print([{"name": m.name, "age": m.age} for m in people])
-    return json.dumps([{"name": m.name, "age": m.age} for m in people])
+    #print(request.form['d'])
+    max = def_max
+    min = def_min
+    pattern = request.form['pattern']
+    try:
+        min = int(request.form['min'])
+        max = int(request.form['max'])
+    except ValueError:
+        make_response('{"message":"Incorrect params"}', 400)
+    people = [{"name": m.name, "age": m.age} for m in people 
+                       if m.age > min and m.age < max and (pattern == '' or len(re.findall(pattern, m.name)) != 0)]
+    #TODO: calc stats
+    return json.dumps({'people': people})
 

c4e03b0a4b3ff2d76eabbe9efd5bd9a2f1ef9ae6
dartrhevan <vovasatunkin@mail.ru>
2020-07-15 13:08:32 +0500

    still don't work

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(os.environ.get('PORT', '80'))
+        PORT = int(os.environ.get('PORT', '8080'))
     except ValueError:
-        PORT = 80
+        PORT = 8080
     app.run(HOST, PORT)
 
